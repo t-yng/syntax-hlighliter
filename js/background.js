9bfb4af190386b435b7484e0261c4cbbc46b70ff
tomohiro yanagi <t.yng.greenmoneky@gmail.com>
2016-05-15 01:45:17 +0900

    オプションでスタイルを変更した時にハイライト表示がおかしくなるバグを修正

--- js/background.js
+++ js/background.js
@@ -3,9 +3,14 @@
 
 	// タブの更新時のイベント処理を追加
 	chrome.tabs.onUpdated.addListener(showIcon)
-	chrome.tabs.onUpdated.addListener((tabId, _, tab) => {
-		const css = localStorage.getItem('syntax_css')? localStorage.getItem('syntax_css') : 'style/default.min.css'
-		chrome.tabs.insertCSS(tabId, {file: css});
+
+	chrome.extension.onMessage.addListener((request, _, response) => {
+		// オプションの設定値を返す
+		if(request.method == "getSyntaxCSS") {
+			const css = localStorage.getItem('syntax_css')
+			const data = {css: css}
+			return response(data)
+		}
 	})
 
 	// チャットワークのタブの時だけアイコンを表示する
